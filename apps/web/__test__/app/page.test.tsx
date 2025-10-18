2ecbeade2fdf2c326b4f80829e422a3bd934d3d2
aldiipratama <paldi0013@gmail.com>
2025-10-18 17:55:19 +0700

    chore: switch testing env with vitest

--- apps/web/__test__/app/page.test.tsx
+++ apps/web/__test__/app/page.test.tsx
@@ -1,23 +1,13 @@
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event"
 import HomePage from "@/app/page";
+import { render, screen } from "@testing-library/react";
+import { describe, expect, test } from "vitest";
 
-describe("HomePage", () => {
-  it("Renders a heading", () => {
-    render(<HomePage />);
+test("Renders a heading", () => {
+  render(<HomePage />);
 
-    const heading = screen.getByRole("heading", { level: 1 });
-
-    expect(heading).toBeInTheDocument();
+  const heading = screen.getByRole("heading", {
+    level: 1
   });
 
-  it("Renders a button", async () => {
-    render(<HomePage />)
-
-    const button = screen.getByRole("button", {name: "Count: 0"})
-
-    await userEvent.click(button)
-    expect(screen.getByText("Count: 1")).toBeInTheDocument()
-  })
+  expect(heading).toBeDefined();
 });
