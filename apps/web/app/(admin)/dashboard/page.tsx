8810538beec2412c10a3cf9540d478e96ca92bb4
rezaa <rezaageng5@gmail.com>
2025-11-01 19:02:03 +0700

    feat(dashboard): add analytics card

--- apps/web/app/(admin)/dashboard/page.tsx
+++ apps/web/app/(admin)/dashboard/page.tsx
@@ -1,10 +1,46 @@
-import { Button } from "@workspace/ui/components/button";
-import Link from "next/link";
+import AnalyticCard from "@/components/dashboard/analytic-card";
+
+const analyticsData = [
+  {
+    title: "Total Revenue",
+    growth: "+12%",
+    value: "Rp177.841.124",
+    description: "Trending up this month",
+  },
+  {
+    title: "Total Units Sold",
+    growth: "+8%",
+    value: "1.234",
+    description: "Trending up this month",
+  },
+  {
+    title: "Avg. Order Value",
+    growth: "+5%",
+    value: "Rp143.567",
+    description: "Stable compared to last month",
+  },
+  {
+    title: "Profit Margin",
+    growth: "-2%",
+    value: "25%",
+    description: "Slight decrease this month",
+  },
+];
 
 export default function DashboardPage() {
   return (
-    <div className="flex h-full w-full items-center justify-center rounded-3xl bg-[#F7FFFB]">
-      dashboard
-    </div>
+    <section>
+      <div className="flex gap-4">
+        {analyticsData.map((data) => (
+          <AnalyticCard
+            key={data.title}
+            title={data.title}
+            growth={data.growth}
+            value={data.value}
+            description={data.description}
+          />
+        ))}
+      </div>
+    </section>
   );
 }
