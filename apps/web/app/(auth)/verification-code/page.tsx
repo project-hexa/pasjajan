a70c92e99a860831f774a68dd96f55457a983cb4
aldiipratama <paldi0013@gmail.com>
2025-11-12 06:34:07 +0700

    feat: slicing verification code

---
+++ apps/web/app/(auth)/verification-code/page.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { verificationCodeSchema } from "@/lib/schema/auth.schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@workspace/ui/components/button";
+import { Card, CardContent, CardTitle } from "@workspace/ui/components/card";
+import {
+  Field,
+  FieldDescription,
+  FieldError,
+  FieldLabel,
+} from "@workspace/ui/components/field";
+import { Input } from "@workspace/ui/components/input";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { Controller, useForm } from "react-hook-form";
+import z from "zod";
+
+export default function VerificationCodePage() {
+  const verifCodeForm = useForm<z.infer<typeof verificationCodeSchema>>({
+    resolver: zodResolver(verificationCodeSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+  const router = useRouter()
+
+  const handleSubmit = (data: z.infer<typeof verificationCodeSchema>) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <Card className="flex w-1/2 flex-col overflow-hidden border-2 border-black">
+        <CardContent className="flex min-h-0 flex-1 flex-col items-center justify-center gap-4 p-0">
+          <Image
+            src="/icon-email-verif-code.svg"
+            alt="Verification Code"
+            width={250}
+            height={250}
+          />
+
+          {!verifCodeForm.formState.isSubmitted ? (
+            <form
+              id="verifCode"
+              onSubmit={verifCodeForm.handleSubmit(handleSubmit)}
+              className="flex flex-col items-center justify-center gap-4"
+            >
+              <Controller
+                control={verifCodeForm.control}
+                name="email"
+                render={({ field, fieldState }) => (
+                  <Field>
+                    <FieldLabel className="sr-only">
+                      Email Verification Code
+                    </FieldLabel>
+                    <FieldDescription className="text-center text-xl">
+                      Masukan Email Anda Untuk Melakukan Verifikasi Kode.
+                    </FieldDescription>
+                    <Input
+                      type="email"
+                      placeholder="Masukan Email"
+                      aria-invalid={fieldState.invalid}
+                      {...field}
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+
+              <Button type="submit" form="verifCode">
+                Kirim
+              </Button>
+            </form>
+          ) : (
+            <>
+            <CardTitle className="text-2xl">Verifikasi Email</CardTitle>
+              <p className="text-center text-xl">
+                Anda telah memasukan <span className="font-bold">{verifCodeForm.getValues("email")}</span> sebagai alamat
+                email untuk akun anda silakan verifikasi alamat email ini dengan
+                mengklik tombol di bawah.
+              </p>
+              <Button onClick={() => router.push("/one-time-password")}>Verifkasi Email</Button>
+            </>
+          )}
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
