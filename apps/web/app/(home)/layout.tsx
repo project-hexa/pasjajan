1bc0c216099bb3a1548618c0f34f921dee092feb
aldiipratama <paldi0013@gmail.com>
2025-11-27 10:52:22 +0700

    chore: add bottom bar for mobile device in home layout

--- apps/web/app/(home)/layout.tsx
+++ apps/web/app/(home)/layout.tsx
@@ -1,14 +1,20 @@
+import { BottomBar } from "@/components/bottom-bar";
 import { Footer } from "@/components/footer";
 import { Navbar } from "@/components/navigation-bar";
 
-export default function HomeLayout({children}: {children: React.ReactNode}) {
+export default function HomeLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <>
       <Navbar className="sticky top-0 z-50" />
-      <main className="overflow-y-auto flex-1">
+      <main className="flex-1 overflow-y-auto">
         {children}
         <Footer />
       </main>
+      <BottomBar />
     </>
-  )
+  );
 }
