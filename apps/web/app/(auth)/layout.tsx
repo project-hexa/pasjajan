1944c88610ebcf4eb7665724b9457da99b8b27ac
aldiipratama <paldi0013@gmail.com>
2025-11-27 10:52:57 +0700

    chore: add bottom bar for mobile device in auth layout

--- apps/web/app/(auth)/layout.tsx
+++ apps/web/app/(auth)/layout.tsx
@@ -1,7 +1,12 @@
+import { BottomBar } from "@/components/bottom-bar";
 import { Footer } from "@/components/footer";
 import { Navbar } from "@/components/navigation-bar";
 
-export default function AuthLayout({children}: {children: React.ReactNode}) {
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <>
       <Navbar className="sticky top-0 z-50" />
@@ -9,6 +14,8 @@ export default function AuthLayout({children}: {children: React.ReactNode}) {
         {children}
         <Footer />
       </main>
+      <BottomBar />
     </>
-  )
+  );
 }
+
