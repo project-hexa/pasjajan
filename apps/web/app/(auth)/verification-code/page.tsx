a82c9eaa28d109ee4509ceec861fc3d799154cc6
aldiipratama <paldi0013@gmail.com>
2025-12-06 19:21:44 +0700

    chore: update icon to iconify and add loader when submitting

--- apps/web/app/(auth)/verification-code/page.tsx
+++ apps/web/app/(auth)/verification-code/page.tsx
@@ -1,15 +1,17 @@
 "use client";
 
+import { useAuth } from "@/hooks/contollers/useAuth";
 import { verificationCodeSchema } from "@/lib/schema/auth.schema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@workspace/ui/components/button";
-import { Card, CardContent, CardTitle } from "@workspace/ui/components/card";
+import { Card, CardContent } from "@workspace/ui/components/card";
 import {
   Field,
   FieldDescription,
   FieldError,
   FieldLabel,
 } from "@workspace/ui/components/field";
+import { Icon } from "@workspace/ui/components/icon";
 import { Input } from "@workspace/ui/components/input";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
@@ -23,15 +25,16 @@ export default function VerificationCodePage() {
       email: "",
     },
   });
-  const router = useRouter()
+  const router = useRouter();
+  const { verificationCode } = useAuth();
 
   const handleSubmit = (data: z.infer<typeof verificationCodeSchema>) => {
-    console.log(data);
+    verificationCode(data);
   };
 
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <Card className="flex lg:w-1/2 md:w-2/3 max-lg:px-5 max-sm:-mt-40 flex-col overflow-hidden md:border-2 md:border-black max-sm:mx-5">
+      <Card className="flex flex-col overflow-hidden max-lg:px-5 max-sm:mx-5 max-sm:-mt-40 md:w-2/3 md:border-2 md:border-black lg:w-1/2">
         <CardContent className="flex min-h-0 flex-1 flex-col items-center justify-center gap-4 p-0">
           <Image
             src="/icon-email-verif-code.svg"
@@ -41,51 +44,47 @@ export default function VerificationCodePage() {
             height={250}
           />
 
-          {!verifCodeForm.formState.isSubmitSuccessful ? (
-            <form
-              id="verifCode"
-              onSubmit={verifCodeForm.handleSubmit(handleSubmit)}
-              className="flex flex-col items-center justify-center gap-4"
-            >
-              <Controller
-                control={verifCodeForm.control}
-                name="email"
-                render={({ field, fieldState }) => (
-                  <Field>
-                    <FieldLabel className="sr-only">
-                      Email Verification Code
-                    </FieldLabel>
-                    <FieldDescription className="text-center text-xl">
-                      Masukan Email Anda Untuk Melakukan Verifikasi Kode.
-                    </FieldDescription>
-                    <Input
-                      type="email"
-                      placeholder="Masukan Email"
-                      aria-invalid={fieldState.invalid}
-                      {...field}
-                    />
-                    {fieldState.invalid && (
-                      <FieldError errors={[fieldState.error]} />
-                    )}
-                  </Field>
-                )}
-              />
+          <form
+            id="verifCode"
+            onSubmit={verifCodeForm.handleSubmit(handleSubmit)}
+            className="flex flex-col items-center justify-center gap-4"
+          >
+            <Controller
+              control={verifCodeForm.control}
+              name="email"
+              render={({ field, fieldState }) => (
+                <Field>
+                  <FieldLabel className="sr-only">
+                    Email Verification Code
+                  </FieldLabel>
+                  <FieldDescription className="text-center text-xl">
+                    Masukan Email Anda Untuk Menerima Verifikasi Kode.
+                  </FieldDescription>
+                  <Input
+                    type="email"
+                    placeholder="Masukan Email"
+                    aria-invalid={fieldState.invalid}
+                    {...field}
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
 
-              <Button type="submit" form="verifCode">
-                Kirim
-              </Button>
-            </form>
-          ) : (
-            <>
-            <CardTitle className="text-2xl">Verifikasi Email</CardTitle>
-              <p className="text-center text-xl">
-                Anda telah memasukan <span className="font-bold">{verifCodeForm.getValues("email")}</span> sebagai alamat
-                email untuk akun anda silakan verifikasi alamat email ini dengan
-                mengklik tombol di bawah.
-              </p>
-              <Button onClick={() => router.push("/one-time-password")}>Verifkasi Email</Button>
-            </>
-          )}
+            <Button type="submit" form="verifCode">
+              {verifCodeForm.formState.isSubmitting ? (
+                <Icon
+                  icon={"lucide:loader-circle"}
+                  width={32}
+                  className="animate-spin"
+                />
+              ) : (
+                "Kirim Kode Verifikasi"
+              )}
+            </Button>
+          </form>
         </CardContent>
       </Card>
     </div>
