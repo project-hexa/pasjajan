0236b89bcd3139e39a6c619b4feee2d2d0eae9e3
DickyDD6 <dickyddermawan06@gmail.com>
2025-12-07 18:53:35 +0700

    fix: remove unused store folder with zustand dependency

--- apps/web/store/usePaymentStore.ts
+++
@@ -1,9 +0,0 @@
-import create from 'zustand'
-
-export interface PaymentStore {
-    // Store properties
-}
-
-export const usePaymentStore = create<PaymentStore>((set, get) => ({
-    // Store implementations
-}))
--- apps/web/store/usePaymentWaitingStore.ts
+++
@@ -1,9 +0,0 @@
-import create from 'zustand'
-
-export interface PaymentWaitingStore {
-    // Store properties
-}
-
-export const usePaymentWaitingStore = create<PaymentWaitingStore>((set, get) => ({
-    // Store implementations
-}))
