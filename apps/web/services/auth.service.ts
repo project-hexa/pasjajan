49a34650efbc0c1fe0593ce43d43ca68948dec02
aldiipratama <paldi0013@gmail.com>
2025-12-17 21:16:04 +0700

    feat: implement auth service with login, registration, OTP handling, and error management

---
+++ apps/web/services/auth.service.ts
@@ -0,0 +1,104 @@
+import {
+  loginSchema,
+  otpSchema,
+  registerSchema,
+  verificationCodeSchema,
+} from "@/lib/schema/auth.schema";
+import { api } from "@/lib/utils/axios";
+import { isAxiosError } from "axios";
+import z from "zod";
+
+export const authService = {
+  login: async (payload: z.infer<typeof loginSchema>) => {
+    try {
+      const { data } = await api.post("/auth/login", {
+        user_identity: payload.email,
+        ...payload,
+      });
+
+      return {
+        ok: true,
+        token: data.data.token,
+        user: data.data.user,
+        message: data.message || "Berhasil Masuk!",
+      };
+    } catch (error) {
+      let message = "Gagal Masuk!";
+
+      if (isAxiosError(error)) {
+        message = error.response?.data?.message ?? message;
+      }
+
+      return {
+        ok: false,
+        message,
+      };
+    }
+  },
+  register: async (payload: z.infer<typeof registerSchema>) => {
+    try {
+      const { data } = await api.post("/auth/register", payload);
+
+      return {
+        ok: true,
+        message: data.message || "Berhasil Daftar Akun!",
+      };
+    } catch (error) {
+      let message = "Gagal Mendaftar!";
+
+      if (isAxiosError(error)) {
+        message = error.response?.data?.message ?? message;
+      }
+
+      return {
+        ok: false,
+        message,
+      };
+    }
+  },
+  sendOTP: async (payload: z.infer<typeof verificationCodeSchema>) => {
+    try {
+      const { data } = await api.post("/auth/send-otp", payload);
+
+      return {
+        ok: true,
+        message: data.message || "OTP Berhasil dikirim!",
+      };
+    } catch (error) {
+      let message = "Logout Gagal!";
+
+      if (isAxiosError(error)) {
+        message = error.response?.data?.message ?? message;
+      }
+
+      return {
+        ok: false,
+        message,
+      };
+    }
+  },
+  verifyOTP: async (payload: z.infer<typeof otpSchema>) => {
+    try {
+      const { data } = await api.post("/auth/verify-otp", {
+        otp: payload.pin,
+        ...payload,
+      });
+
+      return {
+        ok: true,
+        message: data.message || "OTP Berhasil!",
+      };
+    } catch (error) {
+      let message = "OTP Gagal!";
+
+      if (isAxiosError(error)) {
+        message = error.response?.data?.message ?? message;
+      }
+
+      return {
+        ok: false,
+        message,
+      };
+    }
+  },
+};
