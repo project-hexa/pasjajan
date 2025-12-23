bbc7b809961e8bdfb73ad4810cbd378fac229e7a
aldiipratama <paldi0013@gmail.com>
2025-12-23 17:22:31 +0700

    fix(types): make ActionResult generic to support typed data

--- apps/web/types/common.d.ts
+++ apps/web/types/common.d.ts
@@ -1,6 +1,7 @@
-interface ActionResult {
+interface ActionResult<T = unknown> {
   ok: boolean;
   message: string;
+  data?: T;
   description?: string;
   errors?: Record<string, string[]>;
   status?: number;
