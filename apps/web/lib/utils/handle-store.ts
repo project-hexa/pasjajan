1702802d03b3f50e3f50ec1eecf21b229e764140
aldiipratama <paldi0013@gmail.com>
2025-12-23 17:22:14 +0700

    fix(handle-store): update handleStore to support generic return types and optional success message

--- apps/web/lib/utils/handle-store.ts
+++ apps/web/lib/utils/handle-store.ts
@@ -1,14 +1,15 @@
-export const handleStore = async (
-  tryFnc: () => Promise<void>,
-  successMessage: string,
+export const handleStore = async <T = undefined>(
+  tryFnc: () => Promise<T>,
+  successMessage?: string,
   defaultErrorMessage = "Terjadi Kesalahan",
 ) => {
   try {
-    await tryFnc();
+    const data = await tryFnc();
 
     return {
       ok: true,
-      message: successMessage,
+      message: successMessage ?? "",
+      ...(data !== undefined ? { data } : {}),
     };
   } catch (error) {
     const err = error as APIError;
