d500ab2a42dd3cae923836159830b4aa73a988a6
aldiipratama <paldi0013@gmail.com>
2025-12-24 12:53:13 +0700

    feat(user-management): update auth service

--- apps/web/app/(modul 1 - user management)/_services/auth.service.ts
+++ apps/web/app/(modul 1 - user management)/_services/auth.service.ts
@@ -6,46 +6,67 @@ import {
   verifyOTPSchema,
 } from "@/lib/schema/auth.schema";
 import { handleApiRequest } from "@/lib/utils/handle-api-request";
+import { handleApiResponse } from "@/lib/utils/handle-api-response";
 import z from "zod";
 
 export const authService = {
-  login: async (payload: z.infer<typeof loginSchema>): Promise<LoginResponse> =>
-    await handleApiRequest.post<LoginResponse>(
-      "/auth/login",
-      {
-        user_identity: payload.email,
-        ...payload,
-      },
-      {
-        defaultErrorMessage: "Gagal Masuk!",
-      },
+  login: async (payload: Omit<z.infer<typeof loginSchema>, "rememberMe">) =>
+    await handleApiResponse<LoginResponse>(
+      async () =>
+        await handleApiRequest.post<LoginResponse>(
+          "/auth/login",
+          {
+            user_identity: payload.email,
+            password: payload.password,
+            role: payload.role,
+          },
+          {
+            defaultErrorMessage: "Gagal Masuk!",
+          },
+        ),
     ),
   register: async (payload: z.infer<typeof registerSchema>) =>
-    await handleApiRequest.post("/auth/register", payload, {
-      defaultErrorMessage: "Gagal Daftar!",
-    }),
-  sendOTP: async (payload: z.infer<typeof sendOTPSchema>) =>
-    await handleApiRequest.post<SendOTPResponse>("/auth/send-otp", payload, {
-      defaultErrorMessage: "Ada kesalahan saat mengirim OTP!",
-    }),
-  verifyOTP: async (payload: z.infer<typeof verifyOTPSchema>) =>
-    await handleApiRequest.post<VerifyOTPResponse>(
-      "/auth/verify-otp",
-      payload,
-      {
-        defaultErrorMessage: "OTP gagal diverifikasi!",
-      },
+    await handleApiResponse(
+      async () =>
+        await handleApiRequest.post("/auth/register", payload, {
+          defaultErrorMessage: "Gagal Daftar!",
+        }),
     ),
-  logout: async (token: string) =>
-    await handleApiRequest.post(
-      "/auth/logout",
-      { token },
-      {
-        defaultErrorMessage: "Logout Gagal!",
-      },
+  sendOTP: async (payload: z.infer<typeof sendOTPSchema>) =>
+    await handleApiResponse(
+      async () =>
+        await handleApiRequest.post<SendOTPResponse>(
+          "/auth/send-otp",
+          payload,
+          {
+            defaultErrorMessage: "Ada kesalahan saat mengirim OTP!",
+          },
+        ),
+    ),
+  verifyOTP: async (payload: z.infer<typeof verifyOTPSchema>) =>
+    await handleApiResponse(
+      async () =>
+        await handleApiRequest.post<VerifyOTPResponse>(
+          "/auth/verify-otp",
+          payload,
+          {
+            defaultErrorMessage: "OTP gagal diverifikasi!",
+          },
+        ),
+    ),
+  logout: async () =>
+    await handleApiResponse(
+      async () =>
+        await handleApiRequest.post("/auth/logout", {
+          withAuth: true,
+          defaultErrorMessage: "Logout Gagal!",
+        }),
     ),
   resetPassword: async (payload: z.infer<typeof resetPasswordSchema>) =>
-    await handleApiRequest.post("/auth/forgot-password", payload, {
-      defaultErrorMessage: "Gagal mengubah Password!",
-    }),
+    await handleApiResponse(
+      async () =>
+        await handleApiRequest.post("/auth/forgot-password", payload, {
+          defaultErrorMessage: "Gagal mengubah Password!",
+        }),
+    ),
 };
