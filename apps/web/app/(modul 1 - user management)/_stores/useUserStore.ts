b99762ccf3cee7b69164a99c06c4dc8ec59eff6f
aldiipratama <paldi0013@gmail.com>
2025-12-29 15:46:33 +0700

    refactor(userStore): restructure state and action types for clarity

--- apps/web/app/(modul 1 - user management)/_stores/useUserStore.ts
+++ apps/web/app/(modul 1 - user management)/_stores/useUserStore.ts
@@ -2,24 +2,27 @@ import { Customer, User } from "@/types/user";
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
-interface UserStore {
+type State = {
   user: User | null;
   customer: Customer | null;
   isLoggedIn: boolean;
-  setUser: (user: User) => void;
-  setIsLoggedIn: (state: boolean) => void;
-  setCustomer: (customer: Customer) => void;
-}
+};
 
-export const useUserStore = create<UserStore>()(
+type Action = {
+  setUser: (user: User) => void;
+  setCustomer: (customer: Customer) => void;
+  setIsLoggedIn: (state: boolean) => void;
+};
+
+export const useUserStore = create<State & Action>()(
   persist(
     (set) => ({
       user: null,
+      customer: null,
       isLoggedIn: false,
       setUser: (user) => set({ user }),
-      setIsLoggedIn: (state) => set({ isLoggedIn: state }),
-      customer: null,
       setCustomer: (customer) => set({ customer }),
+      setIsLoggedIn: (state) => set({ isLoggedIn: state }),
     }),
     {
       name: "user-store",
